8cf606c1f44e0d2562a5cc3d24159a536f94562a
Grace Tveit <grace.tveit@gmail.com>
2023-08-09 12:17:30 -0700

    docs: outlining approach

--- src/leetcode/longest_common_prefix.rs
+++ src/leetcode/longest_common_prefix.rs
@@ -5,6 +5,20 @@ impl Solution {
     ///
     /// Finds the longest common prefix string among an array of strings. If there
     /// is no common prefix, return an empty string
+    ///
+    /// # Example
+    /// ```rust
+    /// use algos::leetcode::Solution;
+    ///
+    /// let strs = vec![format!("flower"), format!("flow"), format!!("flight")];
+    /// assert_eq!(Solution::longest_common_prefix(strs), "fl");
+    /// ```
+    ///
+    /// # Problem Solving
+    /// The most obvious answer seems to be to index the first character of the
+    /// first string, and check if that is equal to the remaining strings.
+    /// If it is equal, add that to its own string, and repeat until there is
+    /// a single mis-match. At that point, return the new string.
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
         todo!()
     }
